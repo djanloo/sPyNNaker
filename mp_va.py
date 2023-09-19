408bb146dd28a6010b056df6a45369dc24958a1d
djanloo <zzibecu@gmail.com>
2023-09-19 17:03:11 +0200

    added some postproc

--- mp_va.py
+++ mp_va.py
@@ -33,17 +33,17 @@ logging.getLogger("UTILS").setLevel(logging.DEBUG)
 logger = logging.getLogger("APPLICATION")
 logger.setLevel(logging.DEBUG)
 
-min_conn, max_conn = 0.01, 0.08
-N = 10
+min_conn, max_conn = 0.01, 0.03
+N = 3
 
 # Default parameters of each system
-default_params = dict(n_neurons=1000, 
+default_params = dict(n_neurons=600, 
                       exc_conn_p=0.02, 
                       inh_conn_p=0.02,
                       synaptic_delay=2)
 
-# Defines the LunchBox where the systems will be runned on
-lunchbox = LunchBox(sim, timestep=1, 
+# Defines the RunBox where the systems will be runned on
+runbox = LunchBox(sim, timestep=1, 
                     time_scale_factor=50, 
                     duration=1000, 
                     min_delay=2,
@@ -51,7 +51,7 @@ lunchbox = LunchBox(sim, timestep=1,
                     folder=f"n_{default_params['n_neurons']}_conn_scan"
                 )
 
-# Adds a bunch of systems t the lunchbox
+# Adds a bunch of systems t the runbox
 for exc_conn_p in np.linspace(min_conn, max_conn, N):
     for inh_conn_p in np.linspace(min_conn, max_conn, N):
         params = default_params.copy()
@@ -59,7 +59,7 @@ for exc_conn_p in np.linspace(min_conn, max_conn, N):
         params['exc_conn_p'] = exc_conn_p
         params['inh_conn_p'] = inh_conn_p
 
-        lunchbox.add_system(System(build_system, params))
+        runbox.add_system(System(build_system, params))
 
 # Here I specify which variables I want to compute for each population
 # If the function cannot be evaluated a WARING will be raised
@@ -72,22 +72,22 @@ def final_activity(block):
 def final_isi_cv(block):
     return avg_isi_cv(block, t_start=100)
 
-# Here I tell the LunchBox to extract mean_v for each population for each system
-lunchbox.add_extraction(mean_v)
-lunchbox.add_extraction(final_activity)
-lunchbox.add_extraction(final_isi_cv)
+# Here I tell the RunBox to extract mean_v for each population for each system
+runbox.add_extraction(mean_v)
+runbox.add_extraction(final_activity)
+runbox.add_extraction(final_isi_cv)
 
 # Start the simulation & save
-lunchbox.run()
-lunchbox.extract_and_save()
+runbox.run()
+runbox.extract_and_save()
 
-# lunchbox = LunchBox.from_folder(f"n_{default_params['n_neurons']}_conn_scan")
+# runbox = RunBox.from_folder(f"n_{default_params['n_neurons']}_conn_scan")
 
 levels = [np.linspace(-0.1, 110, 20), np.linspace(-0.1, 2, 10) ]
 for extraction, lvls in zip(["final_activity", "final_isi_cv"], levels):
     plt.figure()
     # Extract the data in a format system_id -> function -> population -> values
-    results = lunchbox.get_extraction_triplets("exc_conn_p", "inh_conn_p", extraction)
+    results = runbox.get_extraction_triplets("exc_conn_p", "inh_conn_p", extraction)
 
     logger.info(f"results is {results}")
     
@@ -95,19 +95,36 @@ for extraction, lvls in zip(["final_activity", "final_isi_cv"], levels):
     # Create grid values first.
     min_conn, max_conn = np.min(results['exc']['exc_conn_p', 'inh_conn_p']), np.max(results['exc']['exc_conn_p', 'inh_conn_p'])
     data = results['exc'][extraction]
-    data[np.isnan(data)] = 4.0
 
     triang = tri.Triangulation(*(results['exc']['exc_conn_p', 'inh_conn_p'].T))
+    logger.info(f"Triang has len {len(triang.triangles)}")
+    logger.info(f"Data has len {len(data)}")
+    if extraction == "final_isi_cv":
+        bad_guys = np.isnan(data)
+        data[bad_guys] = 4.0
+    else:
+        bad_guys = data > 110
+ 
+    mask = np.all(np.where(bad_guys[triang.triangles], True, False), axis=1)
+    triang.set_mask(mask)
+
+    # Plot bad points as "X"
+    plt.scatter(*(results['exc']['exc_conn_p', 'inh_conn_p'][bad_guys].T), 
+            color='red', edgecolor="k", marker="X", zorder =10)
+    
+    # Plot the contourf
     mappable=plt.tricontourf(triang, data, levels=lvls)
     
-    plt.scatter(*(results['exc']['exc_conn_p', 'inh_conn_p'].T), 
-                c=results['exc'][extraction], edgecolor="k",vmin=lvls[0], vmax=lvls[-1])
+    # Plot the good points as squares
+    plt.scatter(*(results['exc']['exc_conn_p', 'inh_conn_p'][~bad_guys].T), 
+                c=results['exc'][extraction][~bad_guys], edgecolor="k", marker="s", vmin=lvls[0], vmax=lvls[-1])
     
     plt.colorbar(mappable)
 
+    plt.plot([min_conn, max_conn], [min_conn, max_conn], ls=":", color="k")
     plt.title(f"{extraction} for n_neurons={default_params['n_neurons']}")
     
     plt.xlabel("Excitatory connectivity")
     plt.ylabel("Inhibitory connectivity")
-    plt.savefig(f"{lunchbox.folder}/{extraction}.png")
+    plt.savefig(f"{runbox.folder}/{extraction}.png")
 plt.show()
