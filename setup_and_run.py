eff17706f2245452e0d995303b2d7108de27b6d4
djanloo <zzibecu@gmail.com>
2023-10-03 16:36:16 +0200

    removed useless files

--- setup_and_run.py
+++
@@ -1,6 +0,0 @@
-# Starts my_setup.bash
-# This file is needed for the option 'command' of the client job
-import os
-os.system('chmod +x my_setup.bash ')
-os.system('source my_setup.bash')
-os.system('./my_setup.bash')
